7d3f6324382311819fb9b3fb352ee937cd8d675f
Rajeev <vivek.gdm@gmail.com>
2019-11-03 20:13:54 +0530

    updated

--- automated_scripts/installing_jenkins_m2_java.sh
+++ automated_scripts/installing_jenkins_m2_java.sh
@@ -4,16 +4,13 @@ yum install updates -y
 yum -y install java-1.8.0-openjdk
 
 #Installing MAVEN
-mkdir /opt/maven
-cd /opt/maven
+cd /opt
 # downloading maven version 3.6.0
-wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz
 tar -xvzf apache-maven-3.6.1-bin.tar.gz
 
 #Installing Jenkins
-M2_HOME=/opt/maven/apache-maven-3.6.1
-M2=$M2_HOME/bin
-yum -y install wget
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum -y install jenkins
