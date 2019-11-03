256569a20f20fe9192b23f2fa957cc5718b6cdf3
root <root@ip-172-31-3-89.ap-south-1.compute.internal>
2019-11-03 15:30:06 +0000

    final editing to the file

--- automated_scripts/installing_jenkins_m2_java.sh
+++ automated_scripts/installing_jenkins_m2_java.sh
@@ -14,6 +14,7 @@ cd /opt
 # downloading maven version 3.6.2
 wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz
 tar -xvzf apache-maven-3.6.2-bin.tar.gz
+mv apache-maven-3.6.2 maven
 
 #Installing Jenkins
 echo =============================================================================
@@ -24,5 +25,18 @@ echo ===========================================================================
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum -y install jenkins
+echo -------------------------------------------------------------------------------------------------
+echo  EXPORTING ENVIORNMENTAL VARIABLES
+echo ------------------------------------------------------------------------------------------------
+echo"export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.222.b10-0.amzn2.0.1.x86_64" >>~/.bash_profile
+echo"export M2_HOME=/opt/maven" >>~/.bash_profile
+echo"export M2=/opt/maven/bin" >>~/.bash profile
+echo -----------------------------------------------------------------------------------------------
+echo EXOPRTATION ENDED
+echo -----------------------------------------------------------------------------------------------
+
 service jenkins start
+
+### Remove trailing \r character that causes the error if you editing this file on windows
+#sed -i 's/\r$//'installing_jenkins_m2_java.sh
 
