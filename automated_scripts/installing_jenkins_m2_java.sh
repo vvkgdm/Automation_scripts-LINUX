0480ca80bde9d5dc359b0f698cc8f01986c1b794
root <root@ip-172-31-3-89.ap-south-1.compute.internal>
2019-11-03 15:10:58 +0000

    updated

--- automated_scripts/installing_jenkins_m2_java.sh
+++ automated_scripts/installing_jenkins_m2_java.sh
@@ -11,9 +11,9 @@ echo ===========================================================================
 echo INSTALLING MAVEN
 echo ==============================================================================
 cd /opt
-# downloading maven version 3.6.0
+# downloading maven version 3.6.2
 wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz
-tar -xvzf apache-maven-3.6.1-bin.tar.gz
+tar -xvzf apache-maven-3.6.2-bin.tar.gz
 
 #Installing Jenkins
 echo =============================================================================
@@ -25,3 +25,4 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum -y install jenkins
 service jenkins start
+
